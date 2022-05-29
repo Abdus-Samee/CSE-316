0bd9370d8a53493f0324f096632b9ff69197537a
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-05-30 01:56:16 +0600

    Update Offline  - 1.asm

--- Offline  - 1.asm
+++ Offline  - 1.asm
@@ -3,6 +3,8 @@
 
 .DATA
 X DB ?
+Y DB ?
+Z DB ?
 IDX EQU 1
 ARR DB X DUP(0) 
 
@@ -13,15 +15,18 @@ MAIN PROC
     
     LEA SI, ARR
     MOV BL, IDX
+    MOV Z, '1'
+    AND Z, 0FH
     
     MOV AH, 1
     INT 21H
     MOV CL, AL
-    MOV X, CL
-    AND X, 0FH ;n in integer form
+    MOV X, CL  
+    AND CL, 0FH ;value of input "n" intact for future use
+    AND X, 0FH  ;n in integer form
     INC X
     
-    CMP CL, 30H
+    CMP CL, 0
     JNG EXIT
     JMP INPUT
     
@@ -41,11 +46,48 @@ MAIN PROC
     JMP INPUT
     
     SORT:
-    ;sort the numbers here
-    MOV AX, [SI+3]
+        CMP X, 0
+        JE DISPLAY
+        DEC X
+        ;sort the numbers here
+        MOV DL, [SI+X]          ;key=array[step]
+        MOV BL, X
+        MOV Y, BL
+        SUB Y, 1               ;j=step-1
+        
+            WHILE:
+                ;while(j>=0 and key<array[j])
+                CMP Y, 0
+                JNGE LABEL1
+                MOV BL, Y
+                CMP DL, ARR[BX]
+                JNL LABEL1
+                
+                MOV AL, ARR[BX]
+                MOV BL, Y
+                ADD BX, 1
+                MOV ARR[BX], AL         ;array[j+1]=array[j]
+                SUB Y, 1               ;j=j-1
+                JMP WHILE
+                
+            LABEL1:
+                MOV BL, Y
+                ADD BX, 1
+                MOV ARR[BX], DL       ;array[j+1]=key
+                JMP SORT
     
+    DISPLAY:
+        MOV BL, Z
+        MOV DL, ARR[BX]
+        MOV AH, 2
+        INT 21H
+        INC Z
+        CMP Z, CL
+        JNL EXIT
+        JMP DISPLAY
+        
     EXIT:
-    MOV AH, 4CH
-    INT 21H    
+        MOV AH, 4CH
+        INT 21H    
 MAIN ENDP
 END MAIN
